8540a827db69ab4fb9e78e458b0a93b940a310c6
Alexsandro G Bezerra <aalexo1313@gmail.com>
2020-05-10 07:53:23 -0300

    fix create user

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -4,6 +4,10 @@ module.exports = {
   async store (req, res) {
     const { name, email, password } = req.body
 
+    if (await User.findOne({ where: { email } })) {
+      return res.status(409).json({ error: 'User already created' })
+    }
+
     const user = await User.create({ name, email, password })
 
     user.password = undefined
